b1c51ebdfbd9fa653a25d86e508a1644c63ac6f5
Farzeen Sajeel <farzeensajeel2k04@gmail.com>
2026-05-18 03:06:53 +0200

    First Rest Api Spring Application Completed

---
+++ src/test/java/com/example/firstrestapispring/FirstRestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstrestapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
